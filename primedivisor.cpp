0a526e02b13824561e4e4c340e0701ac5fbb6ea7
Claus Guttesen <kometen@gmail.com>
2014-12-19 16:02:40 +0100

    Use unordered map.

--- primedivisor.cpp
+++ primedivisor.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> divisors;
+unordered_map<int, int> map_divisors;
 
-vector<int> prime(int x, int b, int n) {
+bool IsPrime(int a) {
+	auto b = static_cast<int>(sqrt(a));
+	auto prime = true;
+
+	// Start with three as divisor if n > 2 to save one small step
+	auto x = 2;
+	if (a > 2) {
+		x = 3;
+	}
+
+	for (auto i = x; i <= b; ++i) {
+		if (a % i == 0) {
+			prime = false;
+		}
+	}
+
+	return prime;
+}
+
+unordered_map<int, int> primedivisor(int x, int b, int n) {
 	auto s = 0;
 	auto second = 0;
 	auto loop_prime_divisor = true;
@@ -15,25 +36,27 @@ vector<int> prime(int x, int b, int n) {
 			second = n / s;
 			if (s <= second) {
 				cout << "n = s * t : " << n << " = " << s << " * " << second << endl;
-				divisors.push_back(s);
-				if (s == second) {	// Hence not prime, remove 'parent' product
-					divisors.erase(divisors.end()-2);
-				} else {
-					divisors.push_back(second);
+				if (IsPrime(s)) {
+					map_divisors = {{s, s}};
 				}
-				return prime(x, b, second);
+				if (s != second) {
+					if (IsPrime(second)) {
+						map_divisors = {{second, second}};
+					}
+				}
+				return primedivisor(x, b, second);
 				loop_prime_divisor = false;
 			}
 		}
 	}
-	return divisors;
+	return map_divisors;
 }
 
 int main(int argc, char const *argv[]) {
-	vector<int> v;
+	unordered_map<int, int> mm;
 	auto y = 0;
 	auto is_prime = true;
-	auto n = 245;
+	auto n = 45;
 	auto b = static_cast<int>(sqrt(n));
 
 	// Start with three as divisor if n > 2 to save one small step
@@ -44,13 +67,12 @@ int main(int argc, char const *argv[]) {
 
 	cout << "n: " << n << ", b: " << b << endl;
 
-	v = prime(x, b, n);
+	mm = primedivisor(x, b, n);
 
-	cout << "vector size: " << v.size() << endl;
-
-	for (auto i = 0; i < v.size(); ++i) {
-		cout << v[i] << ",";
+	cout << "mm map contains:";
+	for (auto it = mm.begin(); it != mm.end(); ++it) {
+		cout << " " << it->first << ":" << it->second;
 	}
-
+	cout << endl;
 	return 0;
 }
